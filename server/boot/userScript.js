8790b0204761b7f4ff8ba2f3edfa94853b14419c
Alirëza WJ Arabi <hurricanc@gmail.com>
2018-05-23 11:55:37 +0430

    Add User Script Boot Script Module

---
+++ server/boot/userScript.js
@@ -0,0 +1,82 @@
+var utility = require('../../public/utility')
+var userStatus = require('../../config/userStatus.json')
+
+module.exports = function (app) {
+  var mongoDs = app.dataSources.mongoDs
+
+  var User = app.models.client
+  var Role = app.models.Role
+  var RoleMapping = app.models.RoleMapping
+
+	var time = utility.getUnixTimeStamp()
+	var status = userStatus.available
+
+  var users = [{
+      username: 'alireza',
+      email: 'ceo@6ghadam.com',
+      password: '6GhadamPass',
+			fullname: '6Ghadam Admin',
+      time: time,
+      status: status,
+      emailVerified: true,
+      phoneNumber: '09120001122'
+    },
+    {
+      username: 'support',
+      email: 'support@6ghadam.com',
+      password: '6GhadamSupport1Pass',
+			fullname: '6Ghadam Support',
+      time: time,
+      status: status,
+      emailVerified: true,
+      phoneNumber: '09120001123'
+    }
+  ]
+
+  function createRoles(users) {
+    var role1 = {
+      name: 'founder'
+    }
+
+    Role.create(role1, function (err, role) {
+      if (err)
+        throw err
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[0].id
+      }, function (err, principal) {
+        if (err)
+          throw err
+      })
+    })
+
+    var role2 = {
+      name: 'admin'
+    }
+    Role.create(role2, function (err, role) {
+      if (err)
+        throw err
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[1].id
+      }, function (err, principal) {
+        if (err)
+          throw err
+      })
+    })
+  }
+
+  User.create(users, function (err, users) {
+    if (err) {
+      User.find({'where': {'phoneNumber': '09120001122'}}, function(err, result) {
+        if (err)
+          throw err
+        createRoles(result)
+      })
+    }
+    else {
+      createRoles(users)
+    }
+  })
+
+}
